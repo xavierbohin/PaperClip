0ccea2203075a52cf8e73b317686c17cb63d5a41
xavier bohin <xavier@MacBook-Air-de-Caroline.local>
2021-12-12 17:05:33 +0100

    refacto downloadImgFromUrl to set a default image before request

--- PaperClip/PaperClip/Ressources/UIImageView+Extension.swift
+++ PaperClip/PaperClip/Ressources/UIImageView+Extension.swift
@@ -10,18 +10,14 @@ import UIKit
 extension UIImageView {
     func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
         contentMode = mode
+        self.image = UIImage(systemName: "questionmark.square.dashed")
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
                 let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
                 let data = data, error == nil,
                 let image = UIImage(data: data)
-                else {
-                    DispatchQueue.main.async() { [weak self] in
-                        self?.image = UIImage(systemName: "questionmark.square.dashed")
-                    }
-                    return
-                }
+                else {return}
             DispatchQueue.main.async() { [weak self] in
                 self?.image = image
             }
